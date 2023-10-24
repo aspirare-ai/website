4052654dfbe4594fa562d4dc3a693fe06bdefff4
Mat Dupont <dupont.mat.l@gmail.com>
2023-10-23 22:22:08 -0400

    added sharp support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aspirare.github.io',
-  base: '/website'
+  base: '/website',
+  image: {
+    service: passthroughImageService()
+  }
 })
