3ad66701f8eec584d9e343c69dc1ebd77fc3c3c2
Mat Dupont <dupont.mat.l@gmail.com>
2023-10-24 21:43:59 -0400

    reverted config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aspirare-ai.github.io',
-  base: '/',
+  base: '/website',
   image: {
     service: passthroughImageService()
   }
