9380c5d1ef7306aca84c4765db978c265df5dff6
Mat Dupont <dupont.mat.l@gmail.com>
2023-10-27 00:06:48 -0400

    moved images and svgs, pulled out styles to css files and moved content to .ts files

---
+++ src/content/global.ts
@@ -0,0 +1,11 @@
+export const email = 'info@aspirare.com'
+export const contactSales = 'contact aspirare sales'
+export const navLinks = [
+  { url: '/#products', text: 'products' },
+  { url: '/#about', text: 'about' },
+  { url: '/#careers', text: 'careers' },
+  { url: '/#news', text: 'news' },
+  { url: '/#contact', text: 'contact' }
+]
+export const toc = { url: '/terms-conditions', text: 'Terms & Conditions' }
+export const copyright = 'copyright aspirare 2023'
---
+++ src/content/index.ts
@@ -0,0 +1,81 @@
+const placeholder = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+]
+
+const alphaBody = [
+  'Our first generation AI accelerator brings our innovative analog compute cores to data center applications. The Aspirare Alpha provides up to 2x higher performance at a 4x reduction in energy consumption compared to market standard solutions.',
+  'Aspirare’s driver and software stack is plug and play with the most common ML frameworks, ensuring AI/ML developers and data scientists can leverage Aspirare compute with minimal code changes and performance impacts.'
+]
+
+const betaBody = [
+  'Our next generation AI accelerator presents significant improvements over our previous generation Aspirare Alpha. Innovative architecture improvements now provide comparable performance with flagship AI accelerators in the market with up to 6x reduction in energy consumption. Notably, the Aspirare Beta improves on the Aspirare Alpha performance by up to 5x with incremental improvements on energy consumption.'
+]
+
+const edgeBody = [
+  'Aspirare’s analog computing design is uniquely suited to enable data center quality AI computing directly at the edge. Aspirare Edge can be directly integrated on edge, wearables, mobile and IoT devices empowering AI applications where on-device low-latency processing is critical. Our analog architecture is uniquely suited for small form factor applications, and our reduced energy consumption and thermal dissipation requirements can reduce the size of supporting battery and cooling requirements.'
+]
+
+export const hero = {
+  title: 'Sustainably empowering the AI enabled future.',
+  body: [
+    'What will the AI enabled future look like? Current AI developments are constrained by two major factors - compute capability and the energy required to run current generation AI computing hardware.',
+    'Aspirare aims to empower the next generation of AI with our power efficient AI compute hardware. With edge and data center deployment applications, our products bring next-generation analog computing to the forefront of the AI future.'
+  ]
+}
+
+export const products = {
+  title: 'Products',
+  productCards: [
+    {
+      title: 'Aspirare Alpha',
+      body: placeholder // alphaBody
+    },
+    {
+      title: 'Aspirare Beta',
+      body: placeholder // betaBody
+    },
+    {
+      title: 'Aspirare Edge',
+      body: placeholder //edgeBody
+    }
+  ]
+}
+
+export const about = {
+  title: 'about',
+  body: [
+    'Aspirare is the Latin form of “to aspire” and “to breathe in”. Our mission is to breathe life into the next evolution of AI developments and applications. Our innovative analog approach removes the energy consumption and thermal constraints in place with current generation digital designs, empowering the potential of high-performance compute from data center to edge.'
+  ]
+}
+
+export const careers = {
+  title: 'Careers',
+  body: [
+    'Aspirare is a rapidly growing Canadian startup building a world-class team to advance the development of our analog computing solution. If you’re interested in joining our ambition to empower the AI enabled future, please reach out to info@aspirare.io or view the current job postings at the link below.'
+  ],
+  positions: {
+    text: 'See open positions',
+    url: ''
+  }
+}
+
+export const news = {
+  title: 'News',
+  newsCards: [
+    {
+      title: 'News 1',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore'
+    },
+    {
+      title: 'News 2',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore'
+    }
+  ]
+}
+
+export const contact = {
+  title: 'Contact',
+  body: [
+    'For any inquiries about Aspirare and our products, please reach us at:'
+  ]
+}
