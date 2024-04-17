c3580b0ce511543f0a68c94611e627f13d68bd54
Josh Chan <2879989+chanjoshua@users.noreply.github.com>
2024-04-17 09:31:06 -0400

    Update index.ts

--- src/content/index.ts
+++ src/content/index.ts
@@ -16,10 +16,10 @@ const aiExecution = [
 ]
 
 export const hero = {
-  title: 'Sustainably empowering the AI enabled future.',
+  title: 'Creating a sustainable AI enabled future for Enterprise.',
   body: [
-    'What will the AI enabled future look like? Current AI developments are constrained by two major factors - compute capability and the energy required to run current generation AI computing hardware.',
-    'Aspirare aims to empower the next generation of AI with our power efficient AI compute hardware. With edge and data center deployment applications, our products bring next-generation computing to the forefront of the AI future.'
+    'What will the AI enabled future look like? Aspirare helps organizations leverage generative AI technologies to capture new opportunities in a constantly changing world. Transform faster. Innovate smarter. Anticipate the future.',
+    'Aspirare unlocks the power of artificial intelligence (AI) to help organizations reinvent and reimagine themselves from the ground up to accelerate sustainable new growth.'
   ]
 }
 
@@ -44,14 +44,14 @@ export const services = {
 export const about = {
   title: 'about',
   body: [
-    'Aspirare is the Latin form of “to aspire” and “to breathe in”. Our mission is to breathe life into the next evolution of AI developments and applications. Our innovative approach removes the energy consumption and thermal constraints in place with current generation designs, empowering the potential of high-performance compute from data center to edge.'
+    'Aspirare is the Latin form of “to aspire” and “to breathe in”. Our mission is to breathe life into the next evolution of AI solutions to our customers. Our innovative approach focuses on helping organizations develop and implement lean AI pilots that scale to production so that new value can be realized.'
   ]
 }
 
 export const careers = {
   title: 'Careers',
   body: [
-    'Aspirare is a rapidly growing Canadian startup building a world-class team to advance the development of our vision of creating a sustainable AI computing solution. If you’re interested in joining our ambition to empower the AI enabled future, please reach out to us or view the current job postings at the link below.'
+    'Aspirare is a rapidly growing Canadian startup building a world-class team to advance our vision of creating a sustainable AI consulting firm. If you’re interested in joining our ambition to empower the AI enabled future, please reach out to us or view the current job postings at the link below.'
   ],
   positions: {
     text: 'See open positions',
@@ -63,16 +63,16 @@ export const news = {
   title: 'News',
   newsCards: [
     {
-      title: 'Aspirare joins ventureLAB',
-      body: 'Aspirare has joined ventureLAB - a leading global founder community for hardware technology and enterprise software companies in Canada.'
+      title: 'Aspirare helps client reduce operating cost by $10M annually',
+      body: 'Helped a client to help automate a process that takes contact center calls and processes them. Leveraging GenAI, the new solution reduced costs by more than $10M a year.'
     },
     {
-      title: 'Aspirare files patents',
-      body: 'Aspirare has filed patents for its innovative analog design in AI semiconductors for data center use.'
+      title: 'Aspirare automation with AI to reduce delays from days to minutes',
+      body: 'Worked with a client to optimize service delivery to customers. Created a GenAI tool that reduced delays from days to minutes for closing cases for customers.'
     },
     {
-      title: 'Aspirare completes prototype chip',
-      body: 'Aspirare successfully taped out its first analog multiply engine chip to address AI workloads.'
+      title: 'Aspirare helps client to develop a high-level AI strategy and build out team',
+      body: 'Led a client in developing an AI strategy and plan that works with their corporate goals. Assisted in helping build out the AI team from 0 to 1 and 1 to n.'
     }
   ]
 }
