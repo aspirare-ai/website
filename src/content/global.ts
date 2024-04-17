a705c55ff2c73603be503aea00dfdb24d1f5dc75
Josh Chan <2879989+chanjoshua@users.noreply.github.com>
2024-04-16 22:29:02 -0400

    Update global.ts

--- src/content/global.ts
+++ src/content/global.ts
@@ -1,5 +1,5 @@
 export const email = 'josh@aspirare.io'
-export const contactSales = 'contact aspirare semi sales'
+export const contactSales = 'contact aspirare'
 export const navLinks = [
   { url: '/#products', text: 'products' },
   { url: '/#about', text: 'about' },
@@ -8,4 +8,4 @@ export const navLinks = [
   { url: '/#contact', text: 'contact' }
 ]
 export const toc = { url: '/terms-conditions', text: 'Terms & Conditions' }
-export const copyright = 'copyright aspirare semi 2024'
+export const copyright = 'copyright aspirare 2024'
